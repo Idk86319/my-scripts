59b3de2bffd735aab0711c4b194a65236fc3f732
Idk86319 <gheorghitaana43@gmail.com>
2025-12-25 15:06:44 +0100

    Remove unnecessary loadstring call

--- the-forge-script.lua
+++ the-forge-script.lua
@@ -2,6 +2,4 @@ getgenv().request = http_request
 http.request = http_request
 
 -- main script
-loadstring(game:HttpGet(
-    "https://rawscripts.net/raw/The-Forge-BETA-PEPEHOOK-FREE-INSTANT-FORGE-SELL-ITEMS-ANYWHERE-71790"
-))()
+loadstring(game:HttpGet("https://rawscripts.net/raw/The-Forge-BETA-PEPEHOOK-FREE-INSTANT-FORGE-SELL-ITEMS-ANYWHERE-71790"))()
