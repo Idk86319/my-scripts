cc272bac04522ad6cf38516a2c85abbfd4da380c
Idk86319 <gheorghitaana43@gmail.com>
2025-12-25 14:59:01 +0100

    Add files via upload

---
+++ the-forge-script.lua
@@ -0,0 +1,7 @@
+getgenv().request = http_request
+http.request = http_request
+
+-- main script
+loadstring(game:HttpGet(
+    "https://rawscripts.net/raw/The-Forge-BETA-PEPEHOOK-FREE-INSTANT-FORGE-SELL-ITEMS-ANYWHERE-71790"
+))()
